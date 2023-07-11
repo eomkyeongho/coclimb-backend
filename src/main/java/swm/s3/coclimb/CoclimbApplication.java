9fbd38bfef5564ca602ecf7b5f5ba20ddfe7b48f
강주빈 <jjbin@gangjubin-ui-MacBookAir.local>
2023-07-12 00:18:08 +0900

    first commit

---
+++ src/main/java/swm/s3/coclimb/CoclimbApplication.java
@@ -0,0 +1,13 @@
+package swm.s3.coclimb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoclimbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoclimbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swm/s3/coclimb/CoclimbApplicationTests.java
@@ -0,0 +1,13 @@
+package swm.s3.coclimb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoclimbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
