8c8cb98ed681b54b52365224e2f5c9fbdf8182e0
ekh <52539130+eomkyeongho@users.noreply.github.com>
2023-07-16 21:36:06 +0900

    [Refactor] #1-task-9 InstagramRestApiManager 분리
    - 별도 인스타그램 API 호출하는 클래스로 분리

---
+++ src/main/java/swm/s3/coclimb/api/oauth/instagram/InstagramRestApiManager.java
@@ -0,0 +1,97 @@
+package swm.s3.coclimb.api.oauth.instagram;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
+import swm.s3.coclimb.api.oauth.instagram.dto.LongLivedTokenResponse;
+import swm.s3.coclimb.api.oauth.instagram.dto.ShortLivedTokenResponse;
+
+@Component
+@RequiredArgsConstructor
+public class InstagramRestApiManager {
+
+    private final InstagramWebClient instagramWebClient;
+    private final InstagramOAuthRecord instagramOAuthRecord;
+
+    public ShortLivedTokenResponse getShortLivedAccessTokenAndUserId(String code) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("client_id", instagramOAuthRecord.clientId());
+        formData.add("client_secret", instagramOAuthRecord.clientSecret());
+        formData.add("grant_type", "authorization_code");
+        formData.add("redirect_uri", instagramOAuthRecord.redirectUri());
+        formData.add("code", code);
+
+        String response = instagramWebClient.basicDisplayClient().post()
+                .uri("/oauth/access_token")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .exchangeToMono(clientResponse -> {
+                    if (clientResponse.statusCode().is2xxSuccessful()) {
+                        return clientResponse.bodyToMono(String.class);
+                    } else {
+                        return Mono.error(new RuntimeException("단기 토큰 발급 실패"));
+                    }
+                }).block();
+
+        JSONObject responseJson = new JSONObject(response);
+
+        return ShortLivedTokenResponse.builder()
+                .shortLivedAccessToken(responseJson.getString("access_token"))
+                .userId(responseJson.getLong("user_id"))
+                .build();
+    }
+
+    public LongLivedTokenResponse getLongLivedAccessToken(String shortLivedAccessToken) {
+        String targetUri = String.format("/access_token?grant_type=ig_exchange_token&client_secret=%s&access_token=%s",
+                instagramOAuthRecord.clientSecret(), shortLivedAccessToken);
+
+
+        String response = instagramWebClient.graphClient().get()
+                .uri(targetUri)
+                .exchangeToMono(clientResponse -> {
+                    if (clientResponse.statusCode().is2xxSuccessful()) {
+                        return clientResponse.bodyToMono(String.class);
+                    } else {
+                        return Mono.error(new RuntimeException("장기 토큰 발급 실패"));
+                    }
+                }).block();
+
+        JSONObject responseJson = new JSONObject(response);
+
+        return LongLivedTokenResponse.builder()
+                .longLivedAccessToken(responseJson.getString("access_token"))
+                .tokenType(responseJson.getString("token_type"))
+                .expiresIn(responseJson.getLong("expires_in"))
+                .build();
+    }
+
+    public LongLivedTokenResponse refreshLongLivedToken(String longLivedAccessToken) {
+        String targetUri = String.format("/refresh_access_token?grant_type=ig_refresh_token&access_token=%s",
+                longLivedAccessToken);
+
+        String response = instagramWebClient.graphClient().get()
+                .uri(targetUri)
+                .exchangeToMono(clientResponse -> {
+                    if (clientResponse.statusCode().is2xxSuccessful()) {
+                        return clientResponse.bodyToMono(String.class);
+                    } else {
+                        return Mono.error(new RuntimeException("장기 토큰 갱신 실패"));
+                    }
+                }).block();
+
+        JSONObject responseJson = new JSONObject(response);
+
+        return LongLivedTokenResponse.builder()
+                .longLivedAccessToken(responseJson.getString("access_token"))
+                .tokenType(responseJson.getString("token_type"))
+                .expiresIn(responseJson.getLong("expires_in"))
+                .build();
+    }
+}
