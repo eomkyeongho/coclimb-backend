b90682a61258bbe4eb063000b995ec9147678d35
JJBINY <sisoya0424@gmail.com>
2023-07-26 15:31:13 +0900

    [Config] #28-task-1 JWT설정

--- src/main/java/swm/s3/coclimb/CoclimbApplication.java
+++ src/main/java/swm/s3/coclimb/CoclimbApplication.java
@@ -2,7 +2,10 @@ package swm.s3.coclimb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import swm.s3.coclimb.config.AppConfig;
 
+@EnableConfigurationProperties(AppConfig.class)
 @SpringBootApplication
 public class CoclimbApplication {
 
