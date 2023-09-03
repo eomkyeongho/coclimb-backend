11fd70093f7136da01eb2e61fa474e8a95bd8e43
ekh <52539130+eomkyeongho@users.noreply.github.com>
2023-09-03 15:24:02 +0900

    [Chore] #54 Media @ManyToOne FetchType.LAZY 누락 수정

--- src/main/java/swm/s3/coclimb/domain/media/Media.java
+++ src/main/java/swm/s3/coclimb/domain/media/Media.java
@@ -18,7 +18,7 @@ public class Media extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id")
     private User user;
     private String mediaType;
