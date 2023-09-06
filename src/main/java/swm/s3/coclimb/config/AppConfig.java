862f77be6d52618a3ba367744da236e9faf73225
JJBINY <sisoya0424@gmail.com>
2023-09-06 22:55:52 +0900

    [Config] #65 의존성 및 elasticsearch 관련 설정 수정

--- src/main/java/swm/s3/coclimb/config/AppConfig.java
+++ src/main/java/swm/s3/coclimb/config/AppConfig.java
@@ -1,25 +1,57 @@
 package swm.s3.coclimb.config;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.TransportUtils;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.message.BasicHeader;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import swm.s3.coclimb.api.adapter.out.elasticsearch.ElasticProperties;
 import swm.s3.coclimb.config.aspect.logtrace.LogTraceAspect;
 import swm.s3.coclimb.config.aspect.logtrace.LogTraceImpl;
 import swm.s3.coclimb.config.security.JwtProperties;
 
 @Getter
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
 
     private final JwtProperties jwtProperties;
-
-    public AppConfig(JwtProperties jwtProperties) {
-        this.jwtProperties = jwtProperties;
-    }
+    private final ElasticProperties elasticProperties;
 
     @Bean
     public LogTraceAspect logTraceAspect() {
         return new LogTraceAspect(new LogTraceImpl());
     }
 
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+
+        // Create the low-level client
+        RestClientBuilder restClientBuilder = RestClient
+                .builder(HttpHost.create(elasticProperties.getServerUrl()));
+        RestClient restClient = restClientBuilder
+                .setDefaultHeaders(new Header[]{
+                        new BasicHeader("Authorization", "ApiKey " + elasticProperties.getApiKey())
+                })
+                .setHttpClientConfigCallback(hc -> hc
+                        .setSSLContext(TransportUtils
+                                .sslContextFromCaFingerprint(elasticProperties.getFingerPrint()))
+                )
+                .build();
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+        return new ElasticsearchClient(transport);
+    }
 }
