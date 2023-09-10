f0888c0e3bcbcee2a5a09b4812c144f3c7d0540c
Eom Kyeongho <52539130+eomkyeongho@users.noreply.github.com>
2023-09-11 00:13:27 +0900

    [Fix] #74 InstagramMediaResponseDto 역직렬화 오류 해결 - 기본 생성자 추가

--- src/main/java/swm/s3/coclimb/api/adapter/out/instagram/dto/InstagramMediaResponseDto.java
+++ src/main/java/swm/s3/coclimb/api/adapter/out/instagram/dto/InstagramMediaResponseDto.java
@@ -3,9 +3,10 @@ package swm.s3.coclimb.api.adapter.out.instagram.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class InstagramMediaResponseDto {
 
     @JsonProperty("id")
@@ -18,4 +19,13 @@ public class InstagramMediaResponseDto {
     String thumbnailUrl;
     @JsonProperty("permalink")
     String permalink;
+
+    @Builder
+    public InstagramMediaResponseDto(String mediaId, String mediaType, String mediaUrl, String thumbnailUrl, String permalink) {
+        this.mediaId = mediaId;
+        this.mediaType = mediaType;
+        this.mediaUrl = mediaUrl;
+        this.thumbnailUrl = thumbnailUrl;
+        this.permalink = permalink;
+    }
 }
